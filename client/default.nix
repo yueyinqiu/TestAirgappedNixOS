bee25f8d70fe52f99c0459507a5e20f371c47bb0
yueyinqiu <yueyinqiu@outlook.com>
2026-08-10 21:38:24 +0800

    client add stuff

--- client/default.nix
+++ client/default.nix
@@ -1,7 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [ ]; 
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];  
+  environment.systemPackages = [
+    pkgs.git
+  ];
   users.users.root.initialPassword = "password";
   system.stateVersion = "26.05";
 }
