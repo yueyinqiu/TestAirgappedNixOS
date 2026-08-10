3acfc8b32ab44ed1252681125920e7d2e56dbb41
yueyinqiu <yueyinqiu@outlook.com>
2026-08-10 22:05:03 +0800

    f

--- client/default.nix
+++ client/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
   imports = [ ];
@@ -15,25 +15,6 @@
     isNormalUser = true;
     initialPassword = "password";
     extraGroups = [ "wheel" ];
-  };
-
-  # VM-only settings: second NIC (eth1) on the shared bridge so we can
-  # reach other VMs. The default SLiRP NIC (eth0) is kept for convenience.
-  virtualisation.vmVariant = {
-    virtualisation.qemu.options = [
-      "-netdev tap,id=net-tap,ifname=tap1,script=no,downscript=no"
-      "-device virtio-net-pci,netdev=net-tap,mac=52:54:00:00:00:03"
-    ];
-
-    networking.interfaces.eth1 = {
-      useDHCP = false;
-      ipv4.addresses = [
-        {
-          address = "10.10.10.3";
-          prefixLength = 24;
-        }
-      ];
-    };
   };
 
   system.stateVersion = "26.05";
--- server/default.nix
+++ server/default.nix
@@ -1,27 +1,8 @@
-{ lib, ... }:
+{ ... }:
 
 {
   imports = [ ];
   services.openssh.enable = true;
-
-  # VM-only settings: second NIC (eth1) on the shared bridge so other
-  # VMs can reach us. The default SLiRP NIC (eth0) is kept for convenience.
-  virtualisation.vmVariant = {
-    virtualisation.qemu.options = [
-      "-netdev tap,id=net-tap,ifname=tap0,script=no,downscript=no"
-      "-device virtio-net-pci,netdev=net-tap,mac=52:54:00:00:00:02"
-    ];
-
-    networking.interfaces.eth1 = {
-      useDHCP = false;
-      ipv4.addresses = [
-        {
-          address = "10.10.10.2";
-          prefixLength = 24;
-        }
-      ];
-    };
-  };
 
   users.users.user = {
     isNormalUser = true;
