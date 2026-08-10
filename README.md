fb09973618f4eb225df87aa59b37b051e22b982e
yueyinqiu <yueyinqiu@outlook.com>
2026-08-10 22:27:29 +0800

    add home

--- README.md
+++ README.md
@@ -9,11 +9,14 @@ mv result run/server/vm
 ```
 
 ```
+cd run/server
+QEMU_OPTS="-netdev socket,id=net0,connect=localhost:1234 -device virtio-net-pci,netdev=net0" ./vm/bin/run-nixos-vm
+```
+
+```
 cd run/client
 QEMU_OPTS="-netdev socket,id=net0,listen=:1234 -device virtio-net-pci,netdev=net0" ./vm/bin/run-nixos-vm
 ```
 
-```
-cd run/server
-QEMU_OPTS="-netdev socket,id=net0,connect=localhost:1234 -device virtio-net-pci,netdev=net0" ./vm/bin/run-nixos-vm
-```
+在 client ：
+
--- flake.nix
+++ flake.nix
@@ -3,11 +3,17 @@
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
     };
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
       nixpkgs,
+      home-manager,
       ...
     }:
     {
@@ -19,6 +25,11 @@
       nixosConfigurations.client = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ ./client ];
+      };
+
+      homeConfigurations.home = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [ ./home ];
       };
     };
 }
