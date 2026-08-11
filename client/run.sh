5466a7b4aecda044b642fbcdfc4f024fcd2ad6ec
yueyinqiu <yueyinqiu@outlook.com>
2026-08-11 17:24:41 +0800

    f

--- client/run.sh
+++ client/run.sh
@@ -1,3 +1,4 @@
-NixFodExporter from-installables output .#homeConfigurations.home.activationPackage
+nix derivation show -r .#homeConfigurations.home.activationPackage > d,json
+NixFodExporter from-derivations output d.json
 scp -F /dev/null -r output server@192.168.100.10:/home/server/s
 ssh -F /dev/null server@192.168.100.10 "bash /home/server/s/restore.sh"
