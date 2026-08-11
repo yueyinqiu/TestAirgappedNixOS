9fb70bb32efa9ee1e42538b7bd08422c6b94baa4
yueyinqiu <yueyinqiu@outlook.com>
2026-08-11 22:34:33 +0800

    offline

--- client/run.sh
+++ client/run.sh
@@ -1,1 +1,1 @@
-ssh -F /dev/null server@192.168.100.10 "nix shell --extra-experimental-features flakes --extra-experimental-features nix-command path:/home/server/nixpkgs#hello"
+ssh -F /dev/null server@192.168.100.10 "nix shell --extra-experimental-features flakes --extra-experimental-features nix-command path:/home/server/nixpkgs#hello --offline"
