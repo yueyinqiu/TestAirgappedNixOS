d042e3210479623d4e2c4f955e2f781907262495
yueyinqiu <yueyinqiu@outlook.com>
2026-08-11 21:27:07 +0800

    o

--- client/run.sh
+++ client/run.sh
@@ -1,1 +1,1 @@
-ssh -F /dev/null server@192.168.100.10 "nix shell --override-flake nixpkgs path:/home/server/nixpkgs nixpkgs#hello --offline --extra-experimental-features flakes --extra-experimental-features nix-command"
+ssh -F /dev/null server@192.168.100.10 "nix shell --extra-experimental-features flakes --extra-experimental-features nix-command path:/home/server/nixpkgs#hello --offline"
