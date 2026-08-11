726fd86c6ed83fa7e0c9d02778a6d35be6ff690f
yueyinqiu <yueyinqiu@outlook.com>
2026-08-11 21:07:58 +0800

    f

--- client/run.sh
+++ client/run.sh
@@ -6,4 +6,4 @@ ssh -F /dev/null server@192.168.100.10 "bash /home/server/fod/store.sh"
 
 git clone --depth 1 https://github.com/NixOS/nixpkgs.git
 scp -F /dev/null -r nixpkgs server@192.168.100.10:/home/server/nixpkgs
-ssh -F /dev/null server@192.168.100.10 "nix shell --override-flake nixpkgs path:/home/server/nixpkgs nixpkgs#hello --offline"
+ssh -F /dev/null server@192.168.100.10 "nix shell --override-flake nixpkgs path:/home/server/nixpkgs nixpkgs#hello --offline --extra-experimental-features flakes --extra-experimental-features nix-command"
