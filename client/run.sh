70b39f94aa8eda0f47adbd025231eaa0e4e510c4
yueyinqiu <yueyinqiu@outlook.com>
2026-08-11 21:04:15 +0800

    s

--- client/run.sh
+++ client/run.sh
@@ -1,5 +1,9 @@
-nix derivation show -r .#homeConfigurations.home.activationPackage > derivation.json
+nix derivation show -r nixpkgs#hello > derivation.json
 NixFodExporter from-derivations output derivation.json
-scp -F /dev/null -r . server@192.168.100.10:/home/server/repo
-ssh -F /dev/null server@192.168.100.10 "bash /home/server/repo/store.sh"
-ssh -F /dev/null server@192.168.100.10 "nix build /home/server/repo#homeConfigurations.home.activationPackage"
+
+scp -F /dev/null -r output server@192.168.100.10:/home/server/fod
+ssh -F /dev/null server@192.168.100.10 "bash /home/server/fod/store.sh"
+
+git clone --depth 1 https://github.com/NixOS/nixpkgs.git
+scp -F /dev/null -r nixpkgs server@192.168.100.10:/home/server/nixpkgs
+ssh -F /dev/null server@192.168.100.10 "nix shell --override-flake nixpkgs path:/home/server/nixpkgs nixpkgs#hello --offline"
